b6da3984731b225ef644e3ab2dcb3dc4be545221
ariel biton <arielbiton03@gmail.com>
2024-06-05 17:39:55 +0300

    update

--- ctf/settings.py
+++ ctf/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "ctf_app\static"),
+    os.path.join(BASE_DIR, '/staticfiles', "ctf_app\static"),
 ]
 
 
