48b455310d80544646c83a1a57f64870f3a2d14e
ariel biton <arielbiton03@gmail.com>
2024-06-05 17:25:12 +0300

    UPDATE_FRONT

--- ctf/settings.py
+++ ctf/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Whitenoise to serve static files
 ]
 
 ROOT_URLCONF = 'ctf.urls'
@@ -131,9 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'ctf_app\static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "ctf_app\static"),
 ]
 
 
